c64ffa611c7f22586650f4e63ac39ac71a4a9d65
ALEXIS37ADIGBLI <154370828+ALEXIS37ADIGBLI@users.noreply.github.com>
2025-10-09 16:27:40 +0000

    approche de solution

--- ComptaPerso/config/firebase.ts
+++ ComptaPerso/config/firebase.ts
@@ -1,27 +1,26 @@
-import { Firestore, getFirestore } from 'firebase/firestore';
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-import {initializeAuth, getReactNativePersistence} from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDIGWF-h9-geAk34_k2BEp74z_OugAhZPo",
   authDomain: "compta-perso-c77e6.firebaseapp.com",
   projectId: "compta-perso-c77e6",
-  storageBucket: "compta-perso-c77e6.appspot.com", // ✅ correction ici
+  storageBucket: "compta-perso-c77e6.appspot.com",
   messagingSenderId: "234319412893",
   appId: "1:234319412893:web:4b7cfbc347070311571e74"
 };
 
-// Initialize Firebase
+// Initialiser Firebase App
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-})
+// Initialiser Auth avec AsyncStorage
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 
-export const firestore = getFirestore(app);
+// Initialiser Firestore
+const firestore = getFirestore(app);
+
+export { auth, firestore };
